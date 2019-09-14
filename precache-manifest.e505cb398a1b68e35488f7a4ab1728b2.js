5b88518795d398cd0fa927ef622905ab9995e851
Bill Liu <bill.liu.school@gmail.com>
2019-09-14 05:27:57 -0400

    Updates

---
+++ precache-manifest.e505cb398a1b68e35488f7a4ab1728b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560d32c5e5f618d6f4deb789aba64ef4",
+    "url": "/health-on-a-cloud/index.html"
+  },
+  {
+    "revision": "ea8de1fed26daf8030af",
+    "url": "/health-on-a-cloud/static/css/main.4dc46c80.chunk.css"
+  },
+  {
+    "revision": "3ccd2578fb9fefec1275",
+    "url": "/health-on-a-cloud/static/js/2.6e01f373.chunk.js"
+  },
+  {
+    "revision": "ea8de1fed26daf8030af",
+    "url": "/health-on-a-cloud/static/js/main.2fff3eb6.chunk.js"
+  },
+  {
+    "revision": "d8bfede3e5b6d7d520b6",
+    "url": "/health-on-a-cloud/static/js/runtime~main.3d7a513b.js"
+  }
+]);
